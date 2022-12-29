69cf4b2926db18151ea795cab52bee89ca60c503
Bogdan-Pechounov <119967588+Bogdan-Pechounov@users.noreply.github.com>
2022-12-29 15:00:24 -0500

    Update app.test.js

--- api/app.test.js
+++ api/app.test.js
@@ -4,6 +4,6 @@ import app from './app.js'
 describe('GET /', () => {
   test('responds with some default message', async () => {
     const response = await request(app).get('/')
-    expect(response.text.length).toBeGreaterThan(1000)
+    expect(response.text.length).toBeGreaterThan(0)
   })
 })
