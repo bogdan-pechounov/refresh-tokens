6acdb931f558d00e1fef4e4b4821f5f67823dab7
Bogdan-Pechounov <119967588+Bogdan-Pechounov@users.noreply.github.com>
2023-01-09 18:42:27 -0500

    Update passport-setup.js

--- api/src/config/passport-setup.js
+++ api/src/config/passport-setup.js
@@ -9,83 +9,85 @@ const {
   GITHUB_CLIENT_SECRET,
 } = require('./config')
 
-if (!GOOGLE_CLIENT_ID || !GITHUB_CLIENT_ID) return //for github actions, valid id is required
+if (GOOGLE_CLIENT_ID && GITHUB_CLIENT_ID) {
+  //for github actions, valid id is required
 
-passport.use(
-  new GoogleStrategy(
-    {
-      //options
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: `${
-        process.env.NODE_ENV === 'production'
-          ? 'https://refresh-tokens-api.onrender.com'
-          : ''
-      }/auth/google/callback`,
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      //callback
-      const {
-        id,
-        _json: { name, email, picture },
-      } = profile
-      try {
-        //get user
-        const user = await User.findOne({ email })
-        if (user) {
-          done(null, user)
-        } else {
-          //create new user
-          const newUser = await new User({
-            name,
-            email,
-            profile_picture: picture,
-            provider: 'google',
-          }).save()
-          done(null, newUser)
+  passport.use(
+    new GoogleStrategy(
+      {
+        //options
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: `${
+          process.env.NODE_ENV === 'production'
+            ? 'https://refresh-tokens-api.onrender.com'
+            : ''
+        }/auth/google/callback`,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        //callback
+        const {
+          id,
+          _json: { name, email, picture },
+        } = profile
+        try {
+          //get user
+          const user = await User.findOne({ email })
+          if (user) {
+            done(null, user)
+          } else {
+            //create new user
+            const newUser = await new User({
+              name,
+              email,
+              profile_picture: picture,
+              provider: 'google',
+            }).save()
+            done(null, newUser)
+          }
+        } catch (err) {
+          done(err)
         }
-      } catch (err) {
-        done(err)
       }
-    }
+    )
   )
-)
 
-passport.use(
-  new GithubStrategy(
-    {
-      //options
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: `${
-        process.env.NODE_ENV === 'production'
-          ? 'https://refresh-tokens-api.onrender.com'
-          : ''
-      }/auth/github/callback`,
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      //callback
-      const {
-        _json: { id, name, avatar_url, email },
-      } = profile
-      try {
-        //get user
-        const user = await User.findOne({ email })
-        if (user) {
-          done(null, user)
-        } else {
-          //create new user
-          const newUser = await new User({
-            name,
-            email,
-            profile_picture: avatar_url,
-            provider: 'github',
-          }).save()
-          done(null, newUser)
+  passport.use(
+    new GithubStrategy(
+      {
+        //options
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: `${
+          process.env.NODE_ENV === 'production'
+            ? 'https://refresh-tokens-api.onrender.com'
+            : ''
+        }/auth/github/callback`,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        //callback
+        const {
+          _json: { id, name, avatar_url, email },
+        } = profile
+        try {
+          //get user
+          const user = await User.findOne({ email })
+          if (user) {
+            done(null, user)
+          } else {
+            //create new user
+            const newUser = await new User({
+              name,
+              email,
+              profile_picture: avatar_url,
+              provider: 'github',
+            }).save()
+            done(null, newUser)
+          }
+        } catch (err) {
+          done(err)
         }
-      } catch (err) {
-        done(err)
       }
-    }
+    )
   )
-)
+}
